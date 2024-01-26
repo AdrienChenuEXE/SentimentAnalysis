bfd72cc3ee063c6dc5bbcee3e9943c1f68f11eb8
Adrien Chenu <adrienchenudu73@gmail.com>
2024-01-26 02:48:00 -0800

    Final version of application

--- SentimentAnalysis/ContentView.swift
+++ SentimentAnalysis/ContentView.swift
@@ -8,17 +8,127 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var inputText: String = ""
+    @State private var modelOutput: Sentiment? = nil
+    
+    enum Sentiment: String {
+        case positive = "POSITIVE"
+        case negative = "NEGATIVE"
+        case mixed = "MIXED"
+        case neutral = "NEUTRAL"
+
+
+        func getEmoji() -> String{
+            switch self{
+            case .positive:
+                return "😃"
+            case .negative:
+                return "😡"
+            case .mixed:
+                return "🤔"
+            case .neutral:
+                return "😑"
+            }
         }
-        .padding()
+    }
+
+    
+    private func backgroundColor(for sentiment: Sentiment) -> Color {
+        switch sentiment {
+        case .positive:
+            return Color.green
+        case .negative:
+            return Color.red
+        case .mixed:
+            return Color.orange
+        case .neutral:
+            return Color.gray
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Spacer()
+                
+                Text("Entrez une phrase, l'IA va deviner votre sentiment")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding()
+                
+                
+                TextEditor(text: $inputText)
+                    .padding()
+                    .frame(height:200)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .padding(.horizontal, 20)
+                
+                Spacer()
+                
+                Button("Deviner le sentiment") {
+                    classify()
+                }
+                .font(.headline)
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(8.0)
+                
+                Spacer()
+                
+                                
+                if let sentiment = modelOutput{
+                    Text("\(sentiment.rawValue) \(sentiment.getEmoji())")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(backgroundColor(for: sentiment))
+                        .cornerRadius(10)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 40)
+                }
+                
+                Spacer()
+                
+            }
+            .background(LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .top, endPoint: .bottom))
+            .navigationTitle(" 🧠 IA du futur")
+            .navigationBarTitleTextColor(Color.white)
+        }
+    }
+    
+    func classify() {
+        do {
+            let model = try SentimentAnalysis_1(configuration: .init())
+            let prediction = try model.prediction(text: inputText)
+            modelOutput = Sentiment(rawValue: prediction.label)
+        } catch {
+            print("Something went wrong")
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(
+    )
+}
+
+
+extension View {
+    /// Sets the text color for a navigation bar title.
+    /// - Parameter color: Color the title should be
+    ///
+    /// Supports both regular and large titles.
+    @available(iOS 14, *)
+    func navigationBarTitleTextColor(_ color: Color) -> some View {
+        let uiColor = UIColor(color)
+    
+        // Set appearance for both normal and large sizes.
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: uiColor ]
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: uiColor ]
+    
+        return self
+    }
 }
--- SentimentAnalysis/SentimentAnalysisApp.swift
+++ SentimentAnalysis/SentimentAnalysisApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct SentimentAnalysisApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+            )
         }
     }
 }
